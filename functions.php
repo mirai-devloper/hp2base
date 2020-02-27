77caa859c95d0c9771c2a0aabbfff4e098f422f4
kishimoto <kishimoto@mi-rai.co.jp>
2020-02-27 11:12:25 +0900

    Plugin Update Checkerの導入、GitHubから取得するように変更

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
+require 'plugin-update-checker/plugin-update-checker.php';
+$hp2baseUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/nullpon16tera/hp2base',
+	__FILE__,
+	'hp2base',
+);
+$hp2baseUpdateChecker->setAuthentication('381ab76489c990bb19e1c4b770e933be3a90faa0');
+$hp2baseUpdateChecker->getVcsApi()->enableReleaseAssets();
+
 // ブログのコンテンツ幅の最大値（エディタエリアに影響）
 if ( ! isset( $content_width ) )
 	$content_width = 840;
