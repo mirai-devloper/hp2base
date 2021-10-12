9ec53dda6e5827a1968c699c3a96cf18fa99e02f
nullpon <4980756+nullpon16tera@users.noreply.github.com>
2021-10-12 20:03:49 +0900

    細かな微調整

--- template-parts/archive/loop-catalog.php
+++ template-parts/archive/loop-catalog.php
@@ -11,55 +11,55 @@ add_filter('wp_print_footer_scripts', function() {
 			<span class="jp">カタログ</span>
 		</h2>
 
-		<div class="stylist-category">
-			<h3 id="catalogSearchToggle" class="title"><i class="fa fa-list"></i> ヘアスタイルを探す <i class="fa toggle fa-toggle-down"></i></h3>
+    <?php if( have_posts() ) : ?>
+      <div class="stylist-category">
+        <h3 id="catalogSearchToggle" class="title"><i class="fa fa-list"></i> ヘアスタイルを探す <i class="fa toggle fa-toggle-down"></i></h3>
 
-			<?php //hp_stylist_term(); ?>
-			<?php hp_catalog_search(); ?>
-			<?php //get_template_part('template-parts/catalog-search'); ?>
-		</div>
-    <div class="row ">
-      <!-- ヘアスタイル一覧 -->
-      <div class="scroller-body catalog-archive">
-				<?php if( have_posts() ) : ?>
-					<div class="catalog-loop">
-						<?php while( have_posts() ) : the_post(); ?>
-							<div class="catalog-loop__item">
-								<div class="catalog-loop__item-inner">
-									<a href="<?php the_permalink(); ?>" class="catalog-loop__item-link">
-										<div class="thumb">
-											<?php mio_get_catalog_thumb('catalog-loop'); ?>
-										</div>
-										<div class="meta-box">
-											<h2 class="style-name"><i class="fa fa-angle-right"></i><?php the_title_attribute(); ?></h2>
-
-											<hr class="border-catalog">
-
-											<ul class="meta">
-												<li class="manage"><?php hp_stylist_manage(); ?></li>
-												<li class="staff"><?php hp_stylist_name(); ?></li>
-											</ul>
-										</div>
-									</a>
-								</div>
-							</div>
-						<?php endwhile; ?>
-					</div>
-					<?php pagination(); ?>
-        <?php else : ?>
-          <?php if( is_tax('catalog_length') || is_tax('catalog_tag') || is_tax('com_category') ) : ?>
-            <div class="row">
-              <p style="padding: 42px 0;">お探しのヘアカタログが見つかりませんでした。</p>
-            </div>
-          <?php else : ?>
-            <div class="row">
-              <p style="padding: 42px 0;">ヘアカタログの投稿がありません</p>
-            </div>
-          <?php endif; ?>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
+        <?php //hp_stylist_term(); ?>
+        <?php hp_catalog_search(); ?>
+        <?php //get_template_part('template-parts/catalog-search'); ?>
       </div>
-    </div>
+      <div class="row ">
+        <!-- ヘアスタイル一覧 -->
+        <div class="scroller-body catalog-archive">
+          <div class="catalog-loop">
+            <?php while( have_posts() ) : the_post(); ?>
+              <div class="catalog-loop__item">
+                <div class="catalog-loop__item-inner">
+                  <a href="<?php the_permalink(); ?>" class="catalog-loop__item-link">
+                    <div class="thumb">
+                      <?php mio_get_catalog_thumb('catalog-loop'); ?>
+                    </div>
+                    <div class="meta-box">
+                      <h2 class="style-name"><i class="fa fa-angle-right"></i><?php the_title_attribute(); ?></h2>
+
+                      <hr class="border-catalog">
+
+                      <ul class="meta">
+                        <li class="manage"><?php hp_stylist_manage(); ?></li>
+                        <li class="staff"><?php hp_stylist_name(); ?></li>
+                      </ul>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+          </div>
+          <?php pagination(); ?>
+        </div>
+      </div>
+    <?php else : ?>
+      <div class="row">
+        <div class="scroller-body catalog-archive">
+          <?php if( is_tax('catalog_length') || is_tax('catalog_tag') || is_tax('com_category') ) : ?>
+            <p style="padding: 42px 0;text-align:center;">お探しのヘアカタログが見つかりませんでした。</p>
+          <?php else : ?>
+            <p style="padding: 42px 0;text-align:center;">ヘアカタログの投稿がありません</p>
+          <?php endif; ?>
+        </div>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
 <!-- ここまで - コンテンツ -->
--- templates/full-width-page.php
+++ templates/full-width-page.php
@@ -8,26 +8,14 @@
 
 <main id="main" class="layout-main">
 	<?php while (have_posts()) : the_post(); ?>
-		<?php
-			$excerpt = get_the_excerpt();
-		?>
 		<article id="pageFullWidth" class="pfw">
-			<header class="pfw-header">
-				<div class="pfw-container">
-					<h1 class="pfw-header__title"><?php the_title(); ?></h1>
-				</div>
-			</header>
 			<section class="pfw-content">
 				<div class="pfw-content__inner">
 					<div class="pfw-container">
-						<?php if ($excerpt !== '') echo $excerpt; ?>
 						<?php the_content(); ?>
 					</div>
 				</div>
 			</section>
-			<footer class="pfw-footer">
-
-			</footer>
 		</article>
 	<?php endwhile; ?>
 </main>
